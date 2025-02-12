dc57a36b56b0664df915427f0cf132ce7cf385d8
Aman Khare <khareaman754@gmail.com>
2025-02-12 19:39:05 +0530

    feat: Adding no items available in cart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -8,25 +8,29 @@ const Cart = () => {
       <main className="container">
         <h2 className="py-4 text-center">Cart Items</h2>
         <div className="row">
-          {cartItems.map((item) => {
-            return (
-              <div
-                className="col-sm-6 col-md-4 col-lg-3 my-2 w-75 mx-auto"
-                key={item.id}
-              >
-                <div className="card w-100">
-                  <div className="card-body d-flex align-items-center justify-content-around w-100">
-                    <img
-                      src={`https://placehold.co/120x80/${item.color}/white`}
-                      alt="tshirt-image"
-                    />
-                    <h2 className="card-title">{item.name}</h2>
-                    <h4 className="card-text">₹{item.price}</h4>
+          {cartItems.length > 1 ? (
+            cartItems.map((item) => {
+              return (
+                <div
+                  className="col-sm-6 col-md-4 col-lg-3 my-2 w-75 mx-auto"
+                  key={item.id}
+                >
+                  <div className="card w-100">
+                    <div className="card-body d-flex align-items-center justify-content-around w-100">
+                      <img
+                        src={`https://placehold.co/120x80/${item.color}/white`}
+                        alt="tshirt-image"
+                      />
+                      <h2 className="card-title">{item.name}</h2>
+                      <h4 className="card-text">₹{item.price}</h4>
+                    </div>
                   </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })
+          ) : (
+            <h1 className="text-center mt-5">No Items in Cart</h1>
+          )}
         </div>
       </main>
     </>
